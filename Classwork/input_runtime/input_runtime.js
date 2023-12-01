ff3b3b5a6dff1b962b3a4eafff1a57fff4b57eca
aditya <aryan68125@gmail.com>
2023-12-01 18:40:42 +0530

    Lecture 1 JS

---
+++ Classwork/input_runtime/input_runtime.js
@@ -0,0 +1,4 @@
+var value_1 = parseInt(prompt("First no : ", "Enter first number"));
+var value_2 = parseInt(prompt("Second no : ", "Enter second number"));
+var res = value_1 + value_2;
+alert("added value "+res);
---
+++ Classwork/variable_fun_js/variable_fun_js.js
@@ -0,0 +1,4 @@
+var x = 10 ; 
+var y = 20 ;
+var res = x + y ;
+window.alert("res = "+res);
