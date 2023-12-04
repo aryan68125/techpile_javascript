3c5b1c6d1d570d638d7f8c9be53b862933c0ac16
aditya <aryan68125@gmail.com>
2023-12-04 19:54:05 +0530

    Completed how to get and set dropdown items values using js in bootstrap 5 dropdown menu

--- Homework/form/js/custom_js/form_data_js2.js
+++ Homework/form/js/custom_js/form_data_js2.js
@@ -12,14 +12,6 @@ function get_set_value(){
     var email = document.getElementById("inlineFormInputGroup3");
     var email_value = email.value;
     var email_messege = document.getElementById("email_messege");
-    //apply for
-    var apply = document.getElementById("inlineFormInputGroup4");
-    var apply_value = apply.value;
-    var apply_messege = document.getElementById("apply_messege");
-    //Training mode
-    var training = document.getElementById("inlineFormInputGroup5");
-    var training_value = training.value;
-    var training_messege = document.getElementById("training_messege");
     //College mode
     var college = document.getElementById("inlineFormInputGroup6");
     var college_value = college.value;
@@ -32,10 +24,95 @@ function get_set_value(){
     mobile_messege.innerHTML = mobile_value;
     //email
     email_messege.innerHTML = email_value;
-    //apply for
-    apply_messege.innerHTML = apply_value;
-    //training mode
-    training_messege.innerHTML = training_value;
     //college mode
     college_messege.innerHTML = college_value;
 }
+
+//gets selected items from Apply for drop down menu and displays it in the p tag below Applyfor dropdown menu
+function printSelectedValue_Apply_for(event) {
+    dropdown_selecteditem = document.getElementById("apply_messege");
+    // Prevent the default behavior of the link
+    event.preventDefault();
+
+    // Get the text content of the selected item
+    var selectedValue = event.target.textContent;
+
+    // Print the selected value to the console
+    console.log(selectedValue);
+    dropdown_selecteditem.innerHTML = selectedValue
+  }
+
+  // Get all the <a> elements inside the dropdown menu
+  var dropdownItems = document.querySelectorAll('.Apply_for_items');
+
+  // Add a click event listener to each <a> element
+  dropdownItems.forEach(function(item) {
+    item.addEventListener('click', printSelectedValue_Apply_for);
+  });
+
+
+//gets selected items from Training mode drop down menu and displays it in the p tag below TrainingMode dropdown menu
+  function printSelectedValue_Training_mode(event) {
+    dropdown_selecteditem = document.getElementById("training_messege");
+    // Prevent the default behavior of the link
+    event.preventDefault();
+
+    // Get the text content of the selected item
+    var selectedValue = event.target.textContent;
+
+    // Print the selected value to the console
+    console.log(selectedValue);
+    dropdown_selecteditem.innerHTML = selectedValue
+  }
+
+  // Get all the <a> elements inside the dropdown menu
+  var dropdownItems = document.querySelectorAll('.training_mode_items');
+
+  // Add a click event listener to each <a> element
+  dropdownItems.forEach(function(item) {
+    item.addEventListener('click', printSelectedValue_Training_mode);
+  });
+
+  //gets selected items from Training session drop down menu and displays it in the p tag below TrainingSession dropdown menu
+  function printSelectedValue_Training_session(event) {
+    dropdown_selecteditem = document.getElementById("training_session_messege");
+    // Prevent the default behavior of the link
+    event.preventDefault();
+
+    // Get the text content of the selected item
+    var selectedValue = event.target.textContent;
+
+    // Print the selected value to the console
+    console.log(selectedValue);
+    dropdown_selecteditem.innerHTML = selectedValue
+  }
+
+  // Get all the <a> elements inside the dropdown menu
+  var dropdownItems = document.querySelectorAll('.training_session_items');
+
+  // Add a click event listener to each <a> element
+  dropdownItems.forEach(function(item) {
+    item.addEventListener('click', printSelectedValue_Training_session);
+  });
+
+    //gets selected items from Select_Courses drop down menu and displays it in the p tag below Select_Courses dropdown menu
+    function printSelectedValue_Select_Courses(event) {
+        dropdown_selecteditem = document.getElementById("Select_Courses_messege");
+        // Prevent the default behavior of the link
+        event.preventDefault();
+    
+        // Get the text content of the selected item
+        var selectedValue = event.target.textContent;
+    
+        // Print the selected value to the console
+        console.log(selectedValue);
+        dropdown_selecteditem.innerHTML = selectedValue
+      }
+    
+      // Get all the <a> elements inside the dropdown menu
+      var dropdownItems = document.querySelectorAll('.Select_Courses_items');
+    
+      // Add a click event listener to each <a> element
+      dropdownItems.forEach(function(item) {
+        item.addEventListener('click', printSelectedValue_Select_Courses);
+      });
