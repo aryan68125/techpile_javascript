6a08d63d1c38854db1a63e5841dcd8fc5e133b98
aditya <aryan68125@gmail.com>
2023-12-06 09:28:06 +0530

    added password hide show

--- Homework/password_show_hide/pass_show_hide.js
+++ Homework/password_show_hide/pass_show_hide.js
@@ -2,14 +2,19 @@ function demo()
 {
 var x=document.querySelector("input")
 var y=document.querySelector("img")
-if(x.type=="password")
-{
-x.type="text"
-y.src="images/pass_show.png"
+if(x.value != ""){
+    if(x.type=="password")
+    {
+    x.type="text"
+    y.src="images/pass_show.png"
+    }
+    else
+    {
+    x.type="password"
+    y.src="images/pass_hide.png"
+    }
 }
-else
-{
-x.type="password"
-y.src="images/pass_hide.png"
+else{
+    alert("Please enter password and then continue")
 }
 }
