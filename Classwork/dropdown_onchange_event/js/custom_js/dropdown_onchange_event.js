e19b3e03b3c7b7cfa069124532396d96e6febed9
aditya <aryan68125@gmail.com>
2023-12-07 09:12:49 +0530

    how to open a new window using js

--- Classwork/dropdown_onchange_event/js/custom_js/dropdown_onchange_event.js
+++
@@ -1,4 +0,0 @@
-function dropdown_element_selector(){
-var para = document.querySelector("#para")
-var dropdown = document.querySelector(".Select_Courses_items")
-}
---
+++ Classwork/open_a_new_window/open_new_window.js
@@ -0,0 +1,3 @@
+function open_new_window(){
+    window.open('https://github.com/', '_blank', 'location=yes,height=570,width=520,scrollbars=yes,status=yes');
+}
