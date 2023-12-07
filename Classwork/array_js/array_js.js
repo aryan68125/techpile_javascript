b142c8f83635a9f75bd3f0a53170031912f937e7
aditya <aryan68125@gmail.com>
2023-12-07 11:38:02 +0530

    added for loop normal and for in loop

--- Classwork/array_js/array_js.js
+++ Classwork/array_js/array_js.js
@@ -1,10 +1,22 @@
-var arr = [100,100.56, true, "Techpile"]
+// var arr = [100,100.56, true, "Techpile"]
 // document.write(arr[0]+"<br/>"+arr[1])
 
 //string interpolation 
-document.write(`
-<h1>item of first index : <h1/> ${arr[0]} <br/>
-<h1>item of first index : <h1/> ${arr[1]} <br/>
-<h1>item of first index : <h1/> ${arr[2]} <br/>
-<h1>item of first index : <h1/> ${arr[3]} <br/>
-`)
+// document.write(`
+// <h1>item of first index : <h1/> ${arr[0]} <br/>
+// <h1>item of first index : <h1/> ${arr[1]} <br/>
+// <h1>item of first index : <h1/> ${arr[2]} <br/>
+// <h1>item of first index : <h1/> ${arr[3]} <br/>
+// `);
+
+var studentInfo = ["Rollex", "Bullet", "Ballistic", "Leo"];
+// for (let i = 0; i < studentInfo.length; i++) {
+//     console.log(studentInfo[i]);
+//     alert(studentInfo[i])
+//   }
+
+// for ..in studentInfo
+for(var x in studentInfo){
+    console.log(studentInfo[x]);
+    alert(studentInfo[x])
+}
