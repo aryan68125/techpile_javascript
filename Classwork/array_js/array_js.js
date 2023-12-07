eb054ff1d23b3f1f048321259df56f6c45526c26
aditya <aryan68125@gmail.com>
2023-12-07 10:59:58 +0530

    js array

---
+++ Classwork/array_js/array_js.js
@@ -0,0 +1,10 @@
+var arr = [100,100.56, true, "Techpile"]
+// document.write(arr[0]+"<br/>"+arr[1])
+
+//string interpolation 
+document.write(`
+<h1>item of first index : <h1/> ${arr[0]} <br/>
+<h1>item of first index : <h1/> ${arr[1]} <br/>
+<h1>item of first index : <h1/> ${arr[2]} <br/>
+<h1>item of first index : <h1/> ${arr[3]} <br/>
+`)
